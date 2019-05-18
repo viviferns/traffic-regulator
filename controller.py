bdeebb2f829e2192236a1b473e0174672cf4aac1
Vivian Fernandes <vivianfernandes6795@gmail.com>
2019-05-18 17:17:38 +0530

    thirteenth commit

--- controller.py
+++ controller.py
@@ -1,19 +1,20 @@
 '''File controller'''
 
-from flask import request
+from flask import Flask, redirect, url_for, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/admin_login_control',methods=['POST'])
 def admin_login_control():
 
-    user_name=request.form['user_name']
-    pass_word=request.form['pass_word']
+	user_name=request.form['user_name']
+	pass_word=request.form['pass_word']
 	
 	return render_template('myHtml/home.html')
 
-    '''obj_myDB=myDB()
+'''obj_myDB=myDB()'''
 
-    login_string=obj_myDB.admin_login(user_name,pass_word)
+'''login_string=obj_myDB.admin_login(user_name,pass_word)
 
     if(login_string!="SUCCESSFUL"):
 
@@ -29,51 +30,51 @@ def admin_login_control():
 @app.route('/add_admin_control',methods=['POST'])
 def add_admin_control():
 
-    adm_name=request.form['adm_name']
-    adm_mobNo=request.form['adm_mobNo']
-    adm_userName=request.form['adm_userName']
-    adm_password=request.form['adm_password']
+	adm_name=request.form['adm_name']
+	adm_mobNo=request.form['adm_mobNo']
+	adm_userName=request.form['adm_userName']
+	adm_password=request.form['adm_password']
     
-    verbose="Admin could not be Added"
+	verbose="Admin could not be Added"
 
-    obj_myDB=myDB()
+	obj_myDB=myDB()
 
-    verbose=obj_myDB.add_admin(adm_name,adm_mobNo,adm_userName,adm_password)
+	verbose=obj_myDB.add_admin(adm_name,adm_mobNo,adm_userName,adm_password)
 
-    return render_template('verbose-page.html', verbose)
+	return render_template('verbose-page.html', verbose)
 	
 @app.route('/add_user_control',methods=['POST'])
 def add_user_control():
 
-    name_of_user=request.form['name_of_user']
-    mobile_number=request.form['mobile_number']
-    email_address=request.form['email_address']
-    car_number=request.form['car_number']
+	name_of_user=request.form['name_of_user']
+	mobile_number=request.form['mobile_number']
+	email_address=request.form['email_address']
+	car_number=request.form['car_number']
     
-    verbose="User could not be Added"
+	verbose="User could not be Added"
 
-    obj_myDB=myDB()
+	obj_myDB=myDB()
 
-    verbose=obj_myDB.user_details(name_of_user,mobile_number,email_address,car_number)
+	verbose=obj_myDB.user_details(name_of_user,mobile_number,email_address,car_number)
 
-    return render_template('verbose-page.html', verbose)
+	return render_template('verbose-page.html', verbose)
 	
 @app.route('/admin-temp-password-controller',methods=['POST'])
 def add_admin_control():
 
-    user_name=request.form['user_name']
-    temp_password=request.form['temp_password']
-    re_enter_password=request.form['re_enter_password']
+	user_name=request.form['user_name']
+	temp_password=request.form['temp_password']
+	re_enter_password=request.form['re_enter_password']
 
-    if(temp_password==re_enter_password):
+	if(temp_password==re_enter_password):
 
-        obj_myDB=myDB()
+		obj_myDB=myDB()
 
-        verbose_string=""
+		verbose_string=""
 
-        verbose_string=obj_myDB.update_temp_admin_password(user_name,temp_password)
+		verbose_string=obj_myDB.update_temp_admin_password(user_name,temp_password)
 
-        return render_template('verbose-page.html', verbose_string)
+		return render_template('verbose-page.html', verbose_string)
 
 if __name__ == '__main__':
-    app.run(debug = True)
+	app.run(debug = True)
