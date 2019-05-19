d96f639c46fe12d15e66caaf5a70e32d4945bccd
Vivian Fernandes <vivianfernandes6795@gmail.com>
2019-05-19 12:49:55 +0530

    twentieth commit

--- controller.py
+++ controller.py
@@ -4,7 +4,7 @@ from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-render_template(<p>Successful switch</p>)
+render_template("<p>Successful switch</p>")
 
 @app.route('/admin_login_control',methods=['POST','GET'])
 def admin_login_control():
