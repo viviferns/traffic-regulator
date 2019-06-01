a88464207e8f6fcda8579ad64cf88851c56ad392
Vivian Fernandes <vivianfernandes6795@gmail.com>
2019-06-02 01:45:51 +0530

    forty-fouth commit

--- app.py
+++ app.py
@@ -8,12 +8,29 @@ from flask_sqlalchemy import SQLAlchemy
 db=SQLAlchemy()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
+db=SQLAlchemy(app)
+
+class Person(db.Model):
+
+	ADM_NO=db.Column(db.Integer,primary_key=True)
+	ADMIN_NAME=db.Column(db.String(20),unique=False)
+	MOBILE_NUMBER=db.Column(db.Integer(13),unique=True)
+	ADMIN_USER_NAME=db.Column(db.String(10),unique=True)
+	ADMIN_PASSWORD=db.Column(db.String(16),unique=False)
+	
+	def __init__(self,ADM_NO,ADMIN_NAME,MOBILE_NUMBER,ADMIN_USER_NAME,ADMIN_PASSWORD):
+		self.ADM_NO=ADM_NO
+		self.ADMIN_NAME=ADMIN_NAME
+		self.MOBILE_NUMBER=MOBILE_NUMBER
+		self.ADMIN_USER_NAME=ADMIN_USER_NAME
+		self.ADMIN_PASSWORD=ADMIN_PASSWORD
 
 @app.route('/')
 def index():
 
-	app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
-	db=SQLAlchemy(app)
+	#app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
+	#db=SQLAlchemy(app)
 	
 	# Open database connection
 	#conn=psycopg2.connect(dbname=dbname,user=user,password=password,host=host,port=port)
@@ -22,9 +39,10 @@ def index():
 	#cursor = conn.cursor()
 	
 	#query="select count(ADMIN_USER_NAME) from admin_table"
-	query="CREATE TABLE admin_table(ADM_NO SERIAL PRIMARY KEY NOT NULL, ADMIN_NAME VARCHAR(30) NOT NULL, MOBILE_NUMBER NUMBER NOT NULL,ADMIN_USER_NAME VARCHAR(20) NOT NULL, ADMIN_PASSWORD VARCHAR(16) NOT NULL)"
+	#query="CREATE TABLE admin_table(ADM_NO SERIAL PRIMARY KEY NOT NULL, ADMIN_NAME VARCHAR(30) NOT NULL, MOBILE_NUMBER NUMBER NOT NULL,ADMIN_USER_NAME VARCHAR(20) NOT NULL, ADMIN_PASSWORD VARCHAR(16) NOT NULL)"
 		
 	result=0
+	db.create_all()
 		
 	try:
 		
