e487adf754928802b49f64dd57b82e607fb93aa5
Vivian Fernandes <vivianfernandes6795@gmail.com>
2019-06-02 01:54:42 +0530

    forty-sixth commit

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from flask import Flask, redirect, url_for, render_template, request
 import urllib.parse as urlparse
 from flask_sqlalchemy import SQLAlchemy
 
-db=SQLAlchemy()
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
 db=SQLAlchemy(app)
@@ -15,7 +13,7 @@ class Person(db.Model):
 
 	ADM_NO=db.Column(db.Integer,primary_key=True)
 	ADMIN_NAME=db.Column(db.String(20),unique=False)
-	MOBILE_NUMBER=db.Column(db.Integer(13),unique=True)
+	MOBILE_NUMBER=db.Column(db.Integer,unique=True)
 	ADMIN_USER_NAME=db.Column(db.String(10),unique=True)
 	ADMIN_PASSWORD=db.Column(db.String(16),unique=False)
 	
