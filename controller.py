d06712c81bd114869c61532233369d9df460153a
vivifern <50730250+vivifern@users.noreply.github.com>
2019-06-01 20:59:34 +0530

    test commit for bug removal

--- controller.py
+++ controller.py
@@ -33,9 +33,9 @@ def index():
 		#cursor.close()
 		
 		url = urlparse.urlparse(os.environ.get('DATABASE_URL'))
-		''''db = "dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname)
+		db = "dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname)
 		schema = "schema.sql"
-		conn = psycopg2.connect(db)
+		'''conn = psycopg2.connect(db)
 
 		cur = conn.cursor()
 		
