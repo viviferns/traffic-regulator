fae9743dde712838e6938441e4361a55b8a086e1
Vivian Fernandes <vivianfernandes6795@gmail.com>
2019-06-04 03:17:28 +0530

    resoving admin home page not popping up

--- initDB.py
+++ initDB.py
@@ -23,7 +23,7 @@ class Admins(db.Model):
 	ADMIN_PASSWORD=db.Column(db.String(16),unique=False)
 	
 	def __init__(self,ADM_NO,ADMIN_NAME,MOBILE_NUMBER,ADMIN_USER_NAME,ADMIN_PASSWORD):
-		self.ADM_NO=ADM_NO
+		
 		self.ADMIN_NAME=ADMIN_NAME
 		self.MOBILE_NUMBER=MOBILE_NUMBER
 		self.ADMIN_USER_NAME=ADMIN_USER_NAME
@@ -39,7 +39,7 @@ class Users(db.Model):
 	CAR_NO=db.Column(db.String(10),unique=False)
 	
 	def __init__(self,USR_NO,NAME_OF_USER,MOBILE_NUMBER,EMAIL_ID,CAR_NO):
-		self.USR_NO=USR_NO
+		
 		self.NAME_OF_USER=NAME_OF_USER
 		self.MOBILE_NUMBER=MOBILE_NUMBER
 		self.EMAIL_ID=EMAIL_ID
@@ -51,7 +51,7 @@ class Locations(db.Model):
 	LOC_NAME=db.Column(db.String(20),unique=True)
 	
 	def __init__(self,LOC_NO,LOC_NAME,MOBILE_NUMBER,EMAIL_ID,CAR_NO):
-		self.LOC_NO=LOC_NO
+		
 		self.LOC_NAME=LOC_NAME
 		
 class Violations(db.Model):
@@ -63,7 +63,7 @@ class Violations(db.Model):
 	FINE_AMOUNT=db.Column(db.Integer,unique=False)
 	
 	def __init__(self,REC_NO,TIME_STAMP,CAR_NO,LOC_NAME,VIOLATION_IMAGE,FINE_AMOUNT):
-		self.REC_NO=REC_NO
+		
 		self.TIME_STAMP=TIME_STAMP
 		self.CAR_NO=CAR_NO
 		self.LOC_NAME=LOC_NAME
@@ -80,6 +80,7 @@ class fetch_recod:
 			#from sqlalchemy import and_
 			#login_string=DBSession.query(Admin_table).filter(and_(Admin_table.ADMIN_USER_NAME=username,Admin_table.ADMIN_PASSWORD=password))
 			#return login_string.ADMIN_USER_NAME
+		app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
 		adminsTest=Admins.query.all()
 		return adminsTest.ADMIN_USER_NAME
 
