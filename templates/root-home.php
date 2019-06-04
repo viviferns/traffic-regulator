10734911c0334e88e43234070d155c372840419d
Vivian Fernandes <vivianfernandes6795@gmail.com>
2019-06-04 20:23:27 +0530

    look changes

---
+++ templates/root-home.php
@@ -0,0 +1,26 @@
+<!--this page is only for main admin login page-->
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    </br></br>
+	<a href="templates/addAdmin.html">Add Admins</a>
+	</br></br>
+	<a href="~/templates/updateAdmin.html">Add Admin's Password</a>
+	 </br></br>
+	<a href="/app/templates/removeAdmins.html">Remove Admins</a>
+	</br></br>
+	<a href="templates/addUser">Add Users</a>
+	</br></br>
+	<a href="templates/updateUser">Update User's Details</a>
+	</br></br>
+	<a href="templates/removeUser">Remove Users</a>
+	</br></br>
+	<a href="templates/payment">Fetch User's Dues</a>
+	</br></br>
+	<a href="templates/payment-details">Payment</a>
+	</br></br>
+	<a href="templates/generate-pdf">Send PDF to Users</a>
+	</body>
+</html>
