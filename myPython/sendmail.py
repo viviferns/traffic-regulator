9277231067a02b19c43132359d2785acc8cf2d40
Vivian Fernandes <vivianfernandes6795@gmail.com>
2019-06-14 22:57:59 +0530

    changes for password hider and sendmail

--- myPython/sendmail.py
+++ myPython/sendmail.py
@@ -9,8 +9,8 @@ class SendMail:
 		from_email = Email(email_id)
 		subject = "Test Mail to Display Voilation!"
 		to_email = Email(email_id)
-		str_content="Hi User,\n \n Seems like you have voilated traffic rules! Below are the details:\v Time Stamp \t : \t"+time_stam+"\nCar Number \t : \t"+car_no+"\nLocation \t : \t"+loc_name+"\nFine Amount \t : \t"+fine_amount
-		content = Content("text/plain", )
+		str_content="User,\n \n Seems like you have voilated traffic rules! Below are the details:\v Time Stamp \t : \t"+str(time_stam)+"\nCar Number \t : \t"+car_no+"\nLocation \t : \t"+loc_name+"\nFine Amount \t : \t"+fine_amount
+		content = Content("text/plain", str_content)
 		mail = Mail(from_email, subject, to_email, content)
 		response = sg.client.mail.send.post(request_body=mail.get())
 		print(response.status_code)
