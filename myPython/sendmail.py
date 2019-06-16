5c065fda4139dd5b1cd144674f0c2a3d3fc42afe
Vivian Fernandes <vivianfernandes6795@gmail.com>
2019-06-16 10:06:04 +0530

    changes for password hider and sendmail

--- myPython/sendmail.py
+++ myPython/sendmail.py
@@ -1,11 +1,34 @@
 import sendgrid
 import os
 from sendgrid.helpers.mail import *
+require 'rest-client'
+
 
 class SendMail:
 
 	def send_mail(self,time_stam,car_no,loc_name,fine_amount,email_id,name_of_user):
-		sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
+	
+		API_KEY = os.environ.get('API_KEY')
+		DOMAIN_NAME = os.environ.get('DOMAIN_NAME')
+
+		if not (API_KEY and DOMAIN_NAME):
+			return 'Invalid environment variables'
+
+		result = requests.post(
+			('https://api.mailgun.net/v3/%s/messages' % DOMAIN_NAME),
+			auth = ('api', API_KEY),
+			data = {
+					'from': ('Traffic Department <root@%s>' % DOMAIN_NAME),
+					'to': email_id,
+					'subject': 'Stuff is on sale',
+					'html': name_of_user+',\n \n Seems like you have voilated traffic rules! Below are the details:\v Time Stamp \t : \t'+str(time_stam)+'\nCar Number \t : \t'+car_no+'\nLocation \t : \t'+loc_name+'\nFine Amount \t : \t'+str(fine_amount)
+			}
+		)
+		verbose="Sent Mail to User "+name_of_user
+		return verbose'''
+
+		
+		'''sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
 		from_email = Email(request.form.get("vivianfernandes6795@gmail"))
 		to_email = Email(request.form.get("to_email"))
 		subject = "Test Mail to Display Voilation!"
@@ -18,4 +41,4 @@ class SendMail:
 		print(response.body)
 		print(response.headers)
 		verbose="Sent Mail to User "+name_of_user
-		return verbose
+		return verbose'''
