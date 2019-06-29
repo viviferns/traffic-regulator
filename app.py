8d0e4582f73013674873be984aab6465f6af68e9
Vivian Fernandes <vivianfernandes6795@gmail.com>
2019-06-29 18:12:38 +0530

    Testing mail

--- app.py
+++ app.py
@@ -508,21 +508,21 @@ if __name__ == '__main__':
 	insertUsr=Users(1,"User1",987654321,"testuser123@gmail.com","MH-01-CH-0007")
 	db.session.add(insertUsr)
 	db.session.commit()'''
-	addUsers(1,"User1",9876543212,"vivianfernandes679@gmail.com","KL-01-CC-5919")
-	addUsers(2,"User1",8422065548,"vivianfernandes67@gmail.com","AP-28-DD-2438")
+	addUsers(1,"User1",9876543212,"vivianfernandes6795@gmail.com","KL-01-CC-5919")
+	'''addUsers(2,"User1",8422065548,"vivianfernandes67@gmail.com","AP-28-DD-2438")
 	addUsers(3,"User1",9167316411,"vivianfernandes6@gmail.com","AP-29-AS-8467")
 	addUsers(4,"User1",9769483249,"vivianfernandes@gmail.com","RJ-20-CC-5851")
 	addUsers(5,"User1",9619992079,"vivianfernande@gmail.com","TS-07-EK-7622")
 	addUsers(6,"User1",8286546670,"vivianfernand@gmail.com","AP-10-BC-1485")
 	addUsers(7,"User1",7777721725,"vivianfernan@gmail.com","MH-08-AG-1886")
-	addUsers(8,"User1",8625552718,"vivianferna@gmail.com","AP-36-A-5868")
+	addUsers(8,"User1",8625552718,"vivianferna@gmail.com","AP-36-A-5868")'''
 	addVoilations(1,'KL-01-CC-5919','ChurchGate')
-	addVoilations(2,'AP-28-DD-2438','ChurchGate')
+	'''addVoilations(2,'AP-28-DD-2438','ChurchGate')
 	addVoilations(3,'AP-29-AS-8467','ChurchGate')
 	addVoilations(4,'RJ-20-CC-5851','ChurchGate')
 	addVoilations(5,'TS-07-EK-7622','ChurchGate')
 	addVoilations(6,'AP-10-BC-1485','ChurchGate')
 	addVoilations(7,'MH-08-AG-1886','ChurchGate')
-	addVoilations(8,'AP-36-A-5868','ChurchGate')
+	addVoilations(8,'AP-36-A-5868','ChurchGate')'''
 	addAdmins(1,'MAIN_ADMIN',12345789,'root','ROOT1234')
 	
